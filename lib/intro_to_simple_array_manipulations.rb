16a756c54cf2ec6795f46b1ba416693e9733ec6d
Dzhoy Zuckerman <dzhoyz@gmail.com>
2019-12-03 15:33:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,10 +29,10 @@ def using_concat(my_favorite_things,more_favs)
   more_favs = ["sports cars", "flatiron school"]
   all_my_favs = my_favorite_things.concat(more_favs)
 end
-def using_insert(list_of_programming_languages,element)
+def using_insert(list_of_programming_languages,another_language)
   list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
   another_language = "Python"
-array.insert(3)=element
+  list_of_programming_languages.insert(3)=another_language
 end
 def using_uniq(array)
   array.uniq()
