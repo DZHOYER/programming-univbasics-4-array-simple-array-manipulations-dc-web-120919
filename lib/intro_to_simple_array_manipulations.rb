b6297ca8d46c09662d3fd258fd0061989f5a91ac
Dzhoy Zuckerman <dzhoyz@gmail.com>
2019-12-03 16:00:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ def using_concat(my_favorite_things,more_favs)
 end
 
 def using_insert(list_of_programming_languages,another_language)
-  another_language.insert(3)
+  list_of_programming_languages=another_language.insert(3)
 end
 
 def using_uniq(haircuts)
